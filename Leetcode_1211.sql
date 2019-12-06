648bb06d0504f3c298a9548987440f3109aca8fd
Viniciuscoding <vsg232@nyu.edu>
2019-12-06 09:06:43 -0500

    Queries Quality and Percentage

--- Leetcode_1211.sql
+++ Leetcode_1211.sql
@@ -66,3 +66,13 @@ SELECT query_name,
        ROUND(((COUNT(IF(rating < 3, 1, NULL))/COUNT(*))*100), 2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name
+
+-- ANSWER by Roger_Q
+SELECT query_name,
+       ROUND(AVG(rating/position),2) AS quality,
+       ROUND(AVG(CASE
+                 WHEN rating < 3 THEN 1
+                 ELSE 0 END
+                 ) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name

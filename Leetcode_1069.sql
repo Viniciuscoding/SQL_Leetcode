2332639bcef7a0177508ed9d94d10da608a1953f
Viniciuscoding <vsg232@nyu.edu>
2019-12-09 19:49:48 -0500

    Product Sales Analysis II

--- Leetcode_1069.sql
+++ Leetcode_1069.sql
@@ -63,3 +63,8 @@ FROM Sales s
 INNER JOIN Product p
 ON s.product_id = p.product_id
 GROUP BY s.product_id
+
+# BEST ANSWER: The Product table is unnecessary to solve this problem
+SELECT product_id, SUM(quantity) AS total_quantity
+FROM Sales
+GROUP BY product_id

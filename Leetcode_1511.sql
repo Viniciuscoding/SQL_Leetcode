c20e2cc1753ee8d5ed1aacd24d7794b61dda982f
Viniciuscoding <vin.idem@gmail.com>
2020-08-10 23:29:32 -0400

    Customer Order Frequency - TRY AGAIN

--- Leetcode_1511.sql
+++ Leetcode_1511.sql
@@ -99,3 +99,13 @@ ON o.customer_id = c.customer_id
 GROUP BY o.customer_id
 HAVING SUM(IF(LEFT(o.order_date, 7) = '2020-06', o.quantity, 0) * p.price) >= 100
 AND SUM(IF(LEFT(o.order_date, 7) = '2020-07', o.quantity, 0) * p.price) >= 100
+
+
+Solution by OutOfMemory123
+SELECT customer_id, name
+FROM Customers
+JOIN Orders USING(customer_id) 
+JOIN Product USING(product_id)
+GROUP BY customer_id
+HAVING SUM(IF(LEFT(order_date, 7) = '2020-06', quantity, 0) * price) >= 100
+AND SUM(IF(LEFT(order_date, 7) = '2020-07', quantity, 0) * price) >= 100
